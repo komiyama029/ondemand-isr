1e1d8c6bf8c6f812d67480034ab04017d589abf0
komiyama029 <komiyama@ekanlab.co.jp>
2023-10-06 16:55:19 +0900

    投稿のHTMLをビルド時に出力しないように設定

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -10,14 +10,7 @@ function Post({ post }) {
 }
 
 export async function getStaticPaths() {
-  const res = await axios.get(process.env.NEXT_PUBLIC_API_ENDPOINT);
-  const posts = res.data;
-
-  const paths = posts.map((post) => ({
-    params: { id: post.id.toString() },
-  }));
-
-  return { paths, fallback: "blocking" };
+  return { paths: [], fallback: "blocking" };
 }
 
 export async function getStaticProps({ params }) {
